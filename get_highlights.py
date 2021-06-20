6fdeb9b517896775e0a3f38957a2c8ea1f72ee01
yotam <yotamitai@gmail.com>
2021-06-20 11:36:25 +0300

    updated parser params

--- get_highlights.py
+++ get_highlights.py
@@ -16,8 +16,8 @@ from ffmpeg import merge_and_fade
 
 
 def get_highlights(args):
-    args.output_dir = join(args.results_dir, '_'.join(
-        [basename(args.agent_dir), datetime.now().strftime("%H:%M:%S_%d-%m-%Y")]))
+    args.output_dir = join(abspath('results'), '_'.join(
+        [args.name, datetime.now().strftime("%H:%M:%S_%d-%m-%Y")]))
     make_clean_dirs(args.output_dir)
 
     env, agent, agent_args = get_agent(args)
@@ -87,43 +87,35 @@ def get_highlights(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='RL Agent runner')
-    parser.add_argument('-n', '--num_episodes', help='number of episodes to run', type=int,
-                        default=100)
-    parser.add_argument('-o', '--output', help='directory in which to store results')
-    parser.add_argument('-a', '--agent_dir', help='directory from which to load the agent')
-    parser.add_argument('-c', '--config_file_path', help='path to config file')
-    parser.add_argument('-rv', '--record', help='record videos according to linear schedule',
-                        action='store_true')
+    parser = argparse.ArgumentParser(description='HIGHLIGHTS')
+    parser.add_argument('-a', '--name', help='agent name', type=str, default="Agent-0")
+    parser.add_argument('-num_ep', '--num_episodes', help='number of episodes to run', type=int,
+                        default=1)
+    parser.add_argument('-fps', '--fps', help='summary video fps', type=int, default=1)
+    parser.add_argument('-n', '--n_traces', help='number of traces to obtain', type=int,
+                        default=10)
+    parser.add_argument('-k', '--num_trajectories',
+                        help='number of highlights trajectories to obtain', type=int, default=5)
+    parser.add_argument('-l', '--trajectory_length',
+                        help='length of highlights trajectories ', type=int, default=10)
     parser.add_argument('-v', '--verbose', help='print information to the console',
                         action='store_true')
+    parser.add_argument('-overlapLim', '--overlay_limit', help='# overlaping', type=int,
+                        default=3)
+    parser.add_argument('-minGap', '--minimum_gap', help='minimum gap between trajectories',
+                        type=int, default=0)
+    parser.add_argument('-rand', '--randomized', help='randomize order of summary trajectories',
+                        type=bool, default=True)
+    parser.add_argument('-impMeth', '--importance_type',
+                        help='importance by state or trajectory', default='single_state')
+    parser.add_argument('-impState', '--state_importance',
+                        help='method calculating state importance', default='second')
+    parser.add_argument('-loadTrace', '--load_last_traces',
+                        help='load previously generated traces', type=bool, default=False)
+    parser.add_argument('-loadTraj', '--load_last_trajectories',
+                        help='load previously generated trajectories', type=bool, default=False)
     args = parser.parse_args()
 
-    """agent parameters"""
-    args.agent_dir = abspath('agents/Expert_Mid')
-    args.results_dir = abspath('results')
-    args.num_episodes = 1  # max 2000 (defined in configuration.py)
-    args.fps = 2
-    args.verbose = True
-    args.record = 'all'
-    args.show_score_bar = False
-    args.clear_results = True
-
-    """Highlight parameters"""
-    args.n_traces = 10
-    args.trajectory_importance = "single_state"
-    args.state_importance = "second"
-    args.num_trajectories = 5
-    args.trajectory_length = 10
-    args.fade_duration = 2
-    args.minimum_gap = 0
-    args.overlay_limit = 3
-    args.allowed_similar_states = 3
-    args.highlights_selection_method = 'importance_scores'  # 'scores_and_similarity', 'similarity'
-    args.load_traces = False
-    args.load_trajectories = False
-    args.randomized = True
 
     # RUN
-    args.name = basename(args.agent_dir)
     get_highlights(args)
